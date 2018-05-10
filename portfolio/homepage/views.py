7c452520d4b5be4b06ddc0f05475918cdb78fdef
peter.riser@nielsen.com <peter.riser@nielsen.com>
2018-05-10 09:33:41 -0400

    initial rounds of updates

--- portfolio/homepage/views.py
+++ portfolio/homepage/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render_to_response
- 
+from django.shortcuts import render_to_response, render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
 def index(request):
-    return render_to_response('index.html')
+    return render(request, "index.html")
+
+
+
